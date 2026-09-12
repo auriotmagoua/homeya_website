775a7973aa33a1ee2a17b2673472a9df48fe0b49
auriot magoua <auriotndemagoua@gmail.com>
2026-09-12 01:53:03 +0100

    correct size titre hero section

--- app/components/sections/DownloadBanner.tsx
+++ app/components/sections/DownloadBanner.tsx
@@ -61,8 +61,8 @@ export default function DownloadBanner () {
                   <path d="M3 20.5v-17c0-.83.67-1.5 1.5-1.5.31 0 .61.1.86.28l12.5 8.5c.57.39.71 1.17.32 1.74-.09.13-.2.24-.32.32l-12.5 8.5c-.25.18-.55.28-.86.28-.83 0-1.5-.67-1.5-1.5z" />
                 </svg>
                 <div className="text-left">
-                  <div className="text-[9px] uppercase font-medium leading-none text-gray-300">DISPONIBLE SUR</div>
-                  <div className="text-xs font-semibold leading-tight">Google Play</div>
+                  <div className="text-[10px] uppercase font-medium leading-none text-gray-300">DISPONIBLE SUR</div>
+                  <div className="text-sm font-semibold leading-tight">Google Play</div>
                 </div>
               </Link>
 
@@ -75,8 +75,8 @@ export default function DownloadBanner () {
                   <path d="M18.71 19.5c-.83 1.24-1.71 2.45-3.05 2.47-1.34.03-1.77-.79-3.29-.79-1.53 0-2 .77-3.27.82-1.31.05-2.3-1.32-3.14-2.53C4.25 17 2.94 12.45 4.7 9.39c.87-1.52 2.43-2.48 4.12-2.51 1.28-.02 2.5.87 3.29.87.78 0 2.26-1.07 3.81-.91.65.03 2.47.26 3.64 1.98-.09.06-2.17 1.28-2.15 3.81.03 3.02 2.65 4.03 2.68 4.04-.03.07-.42 1.44-1.38 2.83M15.97 6.32c.62-.76 1.05-1.82.93-2.88-.9.04-2.01.6-2.65 1.35-.57.66-1.07 1.74-.93 2.78 1.01.08 2.03-.49 2.65-1.25z" />
                 </svg>
                 <div className="text-left">
-                  <div className="text-[9px] uppercase font-medium leading-none text-gray-300">Télécharger dans l&apos;</div>
-                  <div className="text-xs font-semibold leading-tight">App Store</div>
+                  <div className="text-[10px] uppercase font-medium leading-none text-gray-300">Disponible Sur</div>
+                  <div className="text-sm font-semibold leading-tight">App Store</div>
                 </div>
               </Link>
             </div>
